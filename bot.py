f039639a00ce288373177cfc8a884b5788da7d66
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2018-09-10 18:35:04 +0300

    Перевел strings в json

--- bot.py
+++ bot.py
@@ -87,19 +87,31 @@ async def restart(message, args):
 
 # Strings go here
 strings = {
-    'desc.none':'Ну, я не знаю, она что-то делает, но что... мне не сказали. Попробуй и узнаешь! :wink:',
-    'embed.author':'Minecraft Бот',
-    'embed.thumbnail':'https://d1u5p3l4wpay3k.cloudfront.net/minecraft_ru_gamepedia/b/bc/Wiki.png?version=26fd08a888d0d1a33fb2808ebc8678e9',
-    'func.help.overflow':'Я твоя не понимать, ты говорить коротко!',
-    'func.help.title':'Помощь по боту',
-    'func.help.specific.title':'Помощь по %s',
-    'func.help.specific.unknown':'Я не знаю, что такое `%s %s`!',
-    'func.kill.failure':'Да кто ты такой?',
-    'func.kill.overflow':'Ты чего, совсем обалдел? Не только пытаешься меня убить, но и грузишь всем этим своим бредом?',
-    'func.kill.success':'Я вернусь! :thumbsup:',
-    'func.none':'Что?',
-    'func.unknown':'Я не понимаю, чего ты от меня хочешь!',
-    'sdesc.none':'Не знаю...'
+    'desc': {
+        'none': 'Ну, я не знаю, она что-то делает, но что... мне не сказали. Попробуй и узнаешь! :wink:'
+    },
+    'embed':{
+        'author': 'Minecraft Бот',
+        'thumbnail': 'https://d1u5p3l4wpay3k.cloudfront.net/minecraft_ru_gamepedia/b/bc/Wiki.png?version=26fd08a888d0d1a33fb2808ebc8678e9'
+    },
+    'func':{
+        'help':{
+            'overflow': 'Я твоя не понимать, ты говорить коротко!',
+            'title': 'Помощь по боту',
+            'specific.title': 'Помощь по %s',
+            'specific.unknown': 'Я не знаю, что такое `%s %s`!'
+        },
+        'kill':{
+            'failure': 'Да кто ты такой?',
+            'overflow': 'Ты чего, совсем обалдел? Не только пытаешься меня убить, но и грузишь всем этим своим бредом?',
+            'success': 'Я вернусь! :thumbsup:'
+        },
+        'none': 'Что?',
+        'unknown': 'Я не понимаю, чего ты от меня хочешь!'        
+    },
+    'sdesc':{
+        'none': 'Не знаю...'
+    }
 }
 # Strings end
 # Commands go here

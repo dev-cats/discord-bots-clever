89f9ec03792ab6d28c464267f9577ac0b20e85f6
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2018-09-10 18:12:11 +0300

    Улучшен log

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
 from datetime import datetime as time
 
-def log(*args, **kwargs):
+def log(value, *args, sep=' ', end='\n'):
     s = time.now().strftime('[%H:%M:%S]')
-    print(s, *args, **kwargs)
-
+    print(s, value, *args, sep=sep, end=end, file=open('log.txt', 'w'))
+    print(s, value, *args, sep=sep, end=end)
+    
 while True:
     with open('bot.py') as f:
         exec(f.read().encode('cp1251').decode('utf-8'))

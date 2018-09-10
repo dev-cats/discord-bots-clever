30d797d3d413be1cc51dfa5d599c97521d615125
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2018-09-10 18:18:03 +0300

    log

--- start.py
+++ start.py
@@ -1,13 +1,9 @@
 from datetime import datetime as time
 
-with open('log.txt', 'x') as f:
-    pass
-
-def log(value, *args, sep=' ', end='\n'):
+def log(*args, **kwargs):
     s = time.now().strftime('[%H:%M:%S]')
-    print(s, value, *args, sep=sep, end=end)
-    print(s, value, *args, sep=sep, end=end, file=open('log.txt', 'w'))
-    
+    print(s, *args, **kwargs)
+
 while True:
     with open('bot.py') as f:
         exec(f.read().encode('cp1251').decode('utf-8'))

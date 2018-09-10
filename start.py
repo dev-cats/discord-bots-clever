008a4a3b5afebe0666f7293b7af022c261e2121c
korochun <catshop@bk.ru>
2018-09-10 17:04:46 +0300

    Убрал пробел

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from datetime import datetime as time
 
 def log(*args, **kwargs):
-    s = time.now().strftime('[%H:%M:%S] ')
+    s = time.now().strftime('[%H:%M:%S]')
     print(s, *args, **kwargs)
 
 while True:
